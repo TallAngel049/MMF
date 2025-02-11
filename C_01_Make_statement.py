bc011d63766e31dc52fc6c0c8e4c4dc01c1b696e
TallAngel049 <159388972+TallAngel049@users.noreply.github.com>
2025-02-12 10:48:31 +1300

    Looping components

---
+++ C_01_Make_statement.py
@@ -0,0 +1,11 @@
+# Functions fo here
+def make_statement(statement, decoration):
+    """Emphasises headings by adding
+    decoration at the start and end"""
+
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+# Main Routine goes here
+
+
+make_statement("Instructions", "ℹ️")
---
+++ C_03_instructions.py
@@ -0,0 +1,65 @@
+# Functions fo here
+def make_statement(statement, decoration):
+    """Emphasises headings by adding
+    decoration at the start and end"""
+
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+
+def string_check(question, valid_answers=('yes', 'no'),
+                 num_letters=1):
+    """Check that users enter the full word
+    or the 'n' letter of a word from a list of valid responses"""
+
+    while True:
+
+        response = input(question).lower()
+
+        for item in valid_answers:
+
+            # Check if the response is the entire work
+            if response == item:
+                return item
+
+            # Check if it's the 'n' letter
+            elif response == item[:num_letters]:
+                return item
+
+        print(f"Please choose an option from {valid_answers}")
+
+
+def instructions():
+    make_statement("Instructions", "ℹ️")
+
+    print('''
+    
+For each ticket holder enter ...
+- Their name
+- Their age
+- The payment method (cash / credit)
+
+The program will record the ticket sale and calculate the 
+ticket cost (and profit).
+
+Once you have either sold all of the tickets or entered the
+exit coe ('xxx'), the program will display the ticket 
+sales information and write data to text file.
+
+It will also choose on lucky ticket holder who wins the 
+draw (their ticket is free).
+
+    ''')
+
+# Main Routine goes here
+
+
+make_statement("Mini-Movie Fundraiser program", "🍿")
+
+print()
+want_instructions = string_check("Do you want to see the instructions? ")
+
+if want_instructions == "yes":
+    instructions()
+
+print()
+print("Program continues")
